ee2537dde579438cfd8d6884645bfa46a952a6f5
Dinesh <dinesh729.dm@gmail.com>
2021-05-15 13:01:00 +0530

    image upload

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -3,6 +3,7 @@ import React, { useState, useContext } from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import {
@@ -39,7 +40,8 @@ const Auth = () => {
       setFormData(
         {
           ...formState.inputs,
-          name: undefined
+          name: undefined,
+          image: undefined
         },
         formState.inputs.email.isValid && formState.inputs.password.isValid
       );
@@ -49,6 +51,10 @@ const Auth = () => {
           ...formState.inputs,
           name: {
             value: '',
+            isValid: false
+          },
+          image: {
+            value: null,
             isValid: false
           }
         },
@@ -82,17 +88,15 @@ const Auth = () => {
     } 
     else {
       try {
+        const formData = new FormData();
+        formData.append('email', formState.inputs.email.value);
+        formData.append('name', formState.inputs.name.value);
+        formData.append('password', formState.inputs.password.value);
+        formData.append('image', formState.inputs.image.value);
         const responseData = await sendRequest(
           'http://localhost:5000/api/users/signup',
           'POST',
-          JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value
-          }),
-          {
-            'Content-Type':'application/json'
-          }
+          formData
         );
         auth.login(responseData.user.id);
 
@@ -121,6 +125,7 @@ const Auth = () => {
             onInput={inputHandler}
           />
         )}
+        {!isLoginMode && <ImageUpload center id='image' onInput={inputHandler} errorText='Please provide an image.' />}
         <Input
           element="input"
           id="email"
